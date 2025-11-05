6655280a4fbf0f0f22858f7dc0445d2e85ce1333
예방 <bok0815@korea.kr>
2025-11-05 12:47:23 +0900

    build4 error

--- src/app/(app)/intro/page.tsx
+++ src/app/(app)/intro/page.tsx
@@ -1,6 +1,7 @@
 import ChairmanMessage from "./ChairmanMessage";
 import IntroContent from "./IntroContent";
 import IntroHeader from "./IntroHeader";
+export const dynamic = 'force-dynamic';
 
 export default function IntroPage() {
   return (
